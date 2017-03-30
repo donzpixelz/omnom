7fd2d86a2eb107cb86678766355147410d78ac81
Nick Featch <Nick.Featch@passivsystems.com>
2017-03-30 13:47:22 +0100

    hide 'Enter the starting point of your API and hit explore.' text on page transition

--- src-cljs/omnom/main.cljs
+++ src-cljs/omnom/main.cljs
@@ -6,10 +6,10 @@
 
 (defn- by-id [id] (.getElementById js/document (name id)))
 
-(when-let [api (get (:query (url/url (-> js/window .-location .-href))) "api")]
-  (set! (.-value (by-id "start-point")) api)
+(if-let [api (get (:query (url/url (-> js/window .-location .-href))) "api")]
   (let [api-url (url/url api)
         port (if-let [p (:port api-url)] (str ":" p) "")
         host (str (:protocol api-url) "://" (:host api-url) port)]
     (set! (.-value (by-id "start-point")) api)
-    (core/omnom api-url (by-id "flibble") host)))
+    (core/omnom api-url (by-id "flibble") host))
+  (set! (.-innerHTML (by-id "flibble")) "Enter the starting point of your API and hit explore."))
