668ce7ff87e42dbb6633001a0ab4693378a84105
Nick Featch <Nick.Featch@passivsystems.com>
2017-05-16 18:04:18 +0100

    allow form to send broken json + improve method filtering

--- src-cljs/omnom/core.cljs
+++ src-cljs/omnom/core.cljs
@@ -30,19 +30,14 @@
     (and (= (count left) (count right)) (<= (- (count left) (count right)) 1))))
 
 (defn- augmented-slurp
-  [uri name aug-req submit-body]
-  (let [clj (when (not (nil? submit-body)) (js->clj (parse submit-body)))
-        base {:with-credentials? false}
-        hdrs (if-let [x (:Authorization (:headers aug-req))]
-               (assoc base :headers {"Authorization" x})
-               base)
-        qps (if-let [x (:query-params aug-req)]
-              (assoc hdrs :query-params x)
-              hdrs)
-        payload (if clj (assoc qps :json-params clj) qps)
-        m-fn (u/http-methods (:method aug-req))]
-    (println "sending:" payload)
-    ((fn [x y] (m-fn x y)) uri payload)))
+  [uri aug-req submit-body]
+  ((u/http-methods (:method aug-req))
+    uri
+    (merge
+      {:with-credentials? false}
+      (when-let [h (:headers aug-req)] {:headers (into {} (for [[k v] h] {(name k) v}))})
+      (when-let [q (:query-params aug-req)] {:query-params q})
+      (when (:body aug-req) {:body submit-body}))))
 
 ;; Public API
 
@@ -52,14 +47,15 @@
     (let [apath (build-analysis-path uri)
           analysis (:body (<! (slurp apath)))
           flt-anal (filter #(and (uri-match? (url/url (:uri %)) uri)
-                                 (or (nil? submit-method) (= (lower-case (:method %)) submit-method)))
+                                 (or (empty? name) (= (lower-case (:method %)) name))
+                                 (or (empty? submit-method) (= (lower-case (:method %)) submit-method)))
                            analysis)
           methods (map #(lower-case (:method %)) flt-anal)
           num (count flt-anal)
           good? (fn [] (and (= num 1) (or (not (nil? submit-method)) (= (first methods) "get"))))
           mkup (cond
                  (= num 0) (b/barf (b/->Error "hal+json") (str apath ": Not found") 404)
-                 (good?)   (let [{:keys [status body]} (<! (augmented-slurp uri name (first flt-anal) submit-body))]
+                 (good?)   (let [{:keys [status body]} (<! (augmented-slurp uri (first flt-anal) submit-body))]
                              (cond
                                (= status 204)    (b/barf (b/->NoContent "hal+json") body status)
                                (success? status) (b/barf (b/->JSONHal "hal+json") body host)
